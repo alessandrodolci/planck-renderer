9f8367277e1eb104543ff70e6aca06ccaaab5f5c
Alessandro Dolci <alessandro.rn@gmail.com>
2019-12-24 23:16:04 +0100

    feat: added text rendering, introduced CanvasContent interface

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,19 +3,31 @@ import { World, Body, Shape } from "planck-js";
 declare module "planck-renderer" {
     export const Renderer: CanvasRenderer | SVGRenderer;
 
+    export interface TextContent {
+        text: string;
+        x: number;
+        y: number;
+    }
+
+    export interface CanvasContent {
+        world: World;
+        textContentArray: TextContent[];
+    }
+
     export class CanvasRenderer {
         private world: World;
+        private textContent: TextContent[];
         private ctx: CanvasRenderingContext2D;
         private canvas: HTMLCanvasElement;
         private options: object;
 
-        constructor(world: World, ctx: CanvasRenderingContext2D, options: object);
+        constructor(canvasContent: CanvasContent, ctx: CanvasRenderingContext2D, options: object);
         private clear(HTMLCanvasElement, CanvasRenderingContext2D): void;
         private drawCircle(body: Body, shape: Shape): void;
         private drawEdge(shape: Shape): void;
         private drawPolygon(body: Body, shape: Shape): void;
         private drawJoint(body: Body, shape: Shape): void;
-        renderWorld(): void;
+        renderContent(): void;
     }
 
     export class SVGRenderer {
